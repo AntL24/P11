ff2026017d41a15392f42907bfb9ad45b5b6be0f
AntL24 <antonin.L2405@gmail.com>
2023-08-28 11:31:39 +0200

    Nav elements underlined when activated

--- kasa/my-app/src/components/Header/Header.js
+++ kasa/my-app/src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
     <header>
       <img src="/images/logo_Kasa.svg" alt="Kasa" className="header-logo" />
       <nav>
-        <Link to="/" className="header-link">Accueil</Link>
-        <Link to="/about" className="header-link">À propos</Link>
+      <NavLink to="/" exact="true"  activeclassname="active-link" className="header-link">Accueil</NavLink>
+        <NavLink to="/about" activeclassname="active-link" className="header-link">À propos</NavLink>
       </nav>
     </header>
   );
